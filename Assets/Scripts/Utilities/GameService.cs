bab0881daaff9aaf922d4dc7004765995130aeae
Yashvardhan4197 <20BCS4197@cuchd.in>
2024-06-28 15:50:16 +0530

    Made all services non mono behaviour and added reference to service locator class(GameService)

--- Assets/Scripts/Utilities/GameService.cs
+++ Assets/Scripts/Utilities/GameService.cs
@@ -1,5 +1,10 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +12,32 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 { 
     public PlayerService playerService {  get; private set; }
+    public SoundService soundService { get; private set; }
+    public EventService eventService { get; private set; }
+    public MapService mapService { get; private set; }
+    public WaveService waveService { get; private set; }
+
+    [SerializeField] private UIService uIService;
+    public UIService UIService=> uIService;
+
     [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
+
+  
 
     void Start()
     {
+        eventService = new EventService();
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
         playerService=new PlayerService(playerScriptableObject);
+        mapService = new MapService(mapScriptableObject);
+        waveService=new WaveService(waveScriptableObject);
+        //eventService=new EventService();
+        UIService.SubscribeToEvents();
     }
 
     void Update()
